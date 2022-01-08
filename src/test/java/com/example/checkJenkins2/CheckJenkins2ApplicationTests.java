b0c266b34f700add822ff4c8900ca0e7984b21c6
DESKTOP-J05IIIT\SAPTARSHI <|Saptarshi1295@gmail.com>
2022-01-08 23:56:37 +0530

    First time created

---
+++ src/test/java/com/example/checkJenkins2/CheckJenkins2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkJenkins2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckJenkins2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
