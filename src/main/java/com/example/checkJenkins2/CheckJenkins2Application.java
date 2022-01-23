99cddc6155f290c96bbb14ee314dde5879ed7b12
SaptarshiP <47676727+SaptarshiP@users.noreply.github.com>
2022-01-23 13:11:28 +0530

    Update CheckJenkins2Application.java

--- src/main/java/com/example/checkJenkins2/CheckJenkins2Application.java
+++ src/main/java/com/example/checkJenkins2/CheckJenkins2Application.java
@@ -8,7 +8,7 @@ public class CheckJenkins2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheckJenkins2Application.class, args);
-		System.out.println("Hello-PSJA2");
+		System.out.println("Hello-PSJA3");
 	}
 
 }
