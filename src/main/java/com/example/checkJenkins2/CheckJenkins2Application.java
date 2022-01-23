7715e0db0d9166c34e9d2cc22b66c62a59f1a71b
SaptarshiP <47676727+SaptarshiP@users.noreply.github.com>
2022-01-23 16:32:51 +0530

    Update CheckJenkins2Application.java

--- src/main/java/com/example/checkJenkins2/CheckJenkins2Application.java
+++ src/main/java/com/example/checkJenkins2/CheckJenkins2Application.java
@@ -8,7 +8,7 @@ public class CheckJenkins2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheckJenkins2Application.class, args);
-		System.out.println("Hello-PSJA7");
+		System.out.println("Hello-PSJA8");
 		System.out.println("This is a dev branch");
 	}
 
